a43056b30d04b60fcab2a3a87872d186d423bfe7
Jaideep Singh Kandari <jaideepskandari@gmail.com>
2017-01-12 12:56:54 +0530

    D8 code import from: https://github.com/JayKandari/shorten

--- shorten.js
+++ shorten.js
@@ -1,10 +1,21 @@
 
-Drupal.behaviors.shorten = function (context) {
-  var input = $('#edit-shortened-url')[0];
-  input.select();
-  input.focus();
-  $('#edit-shortened-url').click(function() {
-    input.select();
-    input.focus();
-  });
-}
+(function ($) {
+
+Drupal.behaviors.shorten = {
+  attach: function (context) {
+    // Make sure we can run context.find().
+    var ctxt = $(context);
+    var shortenedURL = ctxt.find('.shorten-shortened-url');
+    var shorten = shortenedURL[0];
+    if (shorten) {
+      shorten.select();
+      shorten.focus();
+    }
+    shortenedURL.click(function() {
+      this.select();
+      this.focus();
+    });
+  }
+};
+
+})(jQuery);
